663ee1c759bf4fd5999d703bcdcc6e3e76b4a8c7
roman <romko.ua@gmail.com>
2015-01-19 19:43:41 +0200

    Code cleanup.

--- plugin.js
+++ plugin.js
@@ -10,12 +10,11 @@ Selectize.define('add_multiple', function (options) {
   this.setup = (function () {
     var original = self.setup;
     return function () {
-      var items = [];
       original.apply(this, arguments);
 
       this.$control_input.on('paste', function (e) {
         setTimeout(function () {
-          items = $(e.currentTarget).val()
+          $(e.currentTarget).val()
             .split(config.separateBy)
             .map(function (item) {
               return item.trim();
